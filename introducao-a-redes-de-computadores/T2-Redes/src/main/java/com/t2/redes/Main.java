441514c38d04c96a2f56f0d0f8705274751ebbab
Deivid Santos <deividfranciscosantos@hotmail.com>
2022-06-17 11:27:25 -0300

    adição de ping inicial

--- introducao-a-redes-de-computadores/T2-Redes/src/main/java/com/t2/redes/Main.java
+++ introducao-a-redes-de-computadores/T2-Redes/src/main/java/com/t2/redes/Main.java
@@ -26,8 +26,63 @@ public class Main {
         var structure = new Structure();
         structure.build(lines, nodes, routers, routerTables);
 
-        System.out.println(nodes);
-        System.out.println(routers);
-        System.out.println(routerTables);
+        var source = "n1";
+        var dest = "n2";
+
+        var output = new ArrayList<>();
+        var sourceNode = getNodeByName(nodes, source);
+        var destNode = getNodeByName(nodes, dest);
+        String ipToFind = null;
+        if (isAtSameNetwork(sourceNode.ipAddress(), destNode.ipAddress())) {
+            ipToFind = destNode.ipAddress();
+            output.add(noteOverPrint(source, sourceNode.ipAddress(), ipToFind));
+            sourceNode.arpTable().add(new NetInterface(destNode.ipAddress(), destNode.macAddress()));
+            output.add(arpReply(sourceNode.name(), destNode.name(), destNode.ipAddress(), destNode.macAddress()));
+        } else {
+            ipToFind = sourceNode.defaultGateway();
+            output.add(noteOverPrint(source, sourceNode.ipAddress(), ipToFind));
+            for (Router router : routers) {
+                for (NetInterface netInterface : router.netInterfaces()) {
+                    if (cleanIp(netInterface.ip()).equalsIgnoreCase(ipToFind)) {
+                        sourceNode.arpTable().add(netInterface);
+                        output.add(arpReply(sourceNode.name(), router.name(), netInterface.ip(), netInterface.macAddress()));
+                    }
+                }
+            }
+        }
+    }
+
+    private static String arpReply(String sourceName, String destName, String destIp, String destMac) {
+        return String.format("%s ->> %s : ARP Reply<br/>%s is at %s",
+                destName, sourceName, cleanIp(destIp), destMac);
+    }
+
+    private static String noteOverPrint(String sourceName, String sourceIp, String destIp) {
+        var source = cleanIp(sourceIp);
+        return String.format("Note over %s : ARP Request<br/>Who has %s? Tell %s", sourceName, destIp, source);
+    }
+
+    private static String cleanIp(String sourceIp) {
+        return sourceIp.replaceAll("/.*", "");
+    }
+
+    private static boolean isAtSameNetwork(String sourceIp, String destIp) {
+        var splittedIpSource = sourceIp.split("\\.");
+        var splittedIpDest = destIp.split("\\.");
+        for (int i = 0; i < 3; i++) {
+            if (!splittedIpSource[i].equalsIgnoreCase(splittedIpDest[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static Node getNodeByName(ArrayList<Node> nodes, String name) {
+        for (Node node : nodes) {
+            if (node.name().equalsIgnoreCase(name)) {
+                return node;
+            }
+        }
+        throw new RuntimeException("Node not found");
     }
 }
---
+++ introducao-a-redes-de-computadores/T2-Redes/src/main/java/com/t2/redes/Test.java
@@ -0,0 +1,9 @@
+package com.t2.redes;
+
+public class Test {
+    public static void main(String[] args) {
+        String teste = "sfsafsaf";
+        String s = teste.replaceAll("/.*", "");
+        System.out.println(s);
+    }
+}
