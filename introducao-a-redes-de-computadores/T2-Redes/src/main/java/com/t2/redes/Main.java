77558cab17d74b85777c41c234959711957733cd
Deivid Santos <deividfranciscosantos@hotmail.com>
2022-06-19 15:23:15 -0300

    atualização arp

--- introducao-a-redes-de-computadores/T2-Redes/src/main/java/com/t2/redes/Main.java
+++ introducao-a-redes-de-computadores/T2-Redes/src/main/java/com/t2/redes/Main.java
@@ -9,16 +9,26 @@ public class Main {
     public static void main(String[] args) {
         var file = """
                 #NODE
-                n1,00:00:00:00:00:01,192.168.0.2/24,192.168.0.1
-                n2,00:00:00:00:00:02,192.168.0.3/24,192.168.0.1
-                n3,00:00:00:00:00:03,192.168.1.2/24,192.168.1.1
-                n4,00:00:00:00:00:04,192.168.1.3/24,192.168.1.1
+                n1,00:00:00:00:00:01,10.0.0.10/8,10.0.0.1
+                n2,00:00:00:00:00:02,40.0.0.10/8,40.0.0.1
                 #ROUTER
-                r1,2,00:00:00:00:00:05,192.168.0.1/24,00:00:00:00:00:06,192.168.1.1/24
+                r1,3,00:00:00:00:00:10,10.0.0.1/8,00:00:00:00:00:11,20.0.0.1/8,00:00:00:00:00:12,50.0.0.1/8
+                r2,2,00:00:00:00:00:20,20.0.0.2/8,00:00:00:00:00:21,30.0.0.1/8
+                r3,3,00:00:00:00:00:30,30.0.0.2/8,00:00:00:00:00:31,40.0.0.1/8,00:00:00:00:00:32,50.0.0.2/8
                 #ROUTERTABLE
-                r1,192.168.0.0/24,0.0.0.0,0
-                r2,192.168.0.0/24,0.0.0.0,0
-                r1,192.168.1.0/24,0.0.0.0,1""";
+                r1,10.0.0.0/8,0.0.0.0,0
+                r1,20.0.0.0/8,0.0.0.0,1
+                r1,50.0.0.0/8,0.0.0.0,2
+                r1,0.0.0.0/0,20.0.0.2,1
+                r2,20.0.0.0/8,0.0.0.0,0
+                r2,30.0.0.0/8,0.0.0.0,1
+                r2,10.0.0.0/8,20.0.0.1,0
+                r2,0.0.0.0/0,30.0.0.2,1
+                r3,30.0.0.0/8,0.0.0.0,0
+                r3,40.0.0.0/8,0.0.0.0,1
+                r3,50.0.0.0/8,0.0.0.0,2
+                r3,0.0.0.0/0,50.0.0.1,2
+                """;
 
         var lines = Arrays.asList(file.split("\n"));
 
@@ -28,13 +38,13 @@ public class Main {
         var structure = new Structure();
         structure.build(lines, nodes, routers, routerTables);
         // --------------------------------------------------------
-        run(nodes, routers, routerTables);
-    }
-
-    public static List<String> run(List<Node> nodes, List<Router> routers, List<RouterTable> routerTables) {
         String command = "ping";
         var sourceName = "n1";
-        var destName = "n3";
+        var destName = "n2";
+        run(nodes, routers, routerTables, sourceName, destName);
+    }
+
+    public static List<String> run(List<Node> nodes, List<Router> routers, List<RouterTable> routerTables, String sourceName, String destName) {
         var output = new ArrayList<String>();
         var sourceNode = getNodeByName(nodes, sourceName);
         var destNode = getNodeByName(nodes, destName);
@@ -47,39 +57,35 @@ public class Main {
         var sourceMode = HardwareType.NODE;
         boolean replyMode = false;
 
+        boolean end = false;
         for (int i = 0; i < 10; i++) {
+            if (end) {
+                break;
+            }
             if (sourceMode == HardwareType.ROUTER) {
-                for (NetInterface netInterface : currentRouter.netInterfaces()) {
-                    if (isAtSameNetwork(netInterface.ipAddress(), currentDest.ipAddress())) {
-                        if (notContainsInArpTable(currentRouter.arpTable(), currentDest.ipAddress())) {
-                            output.add(arpRequestMessage(currentRouter.name(), currentDest.ipAddress(), netInterface.ipAddress()));
-                            output.add(arpReplyMessage(currentDest.name(), currentRouter.name(), currentDest.ipAddress(), currentDest.macAddress()));
-                            currentRouter.arpTable().add(new NetInterface(currentDest.ipAddress(), currentDest.macAddress()));
-                            currentDest.arpTable().add(new NetInterface(netInterface.ipAddress(), netInterface.macAddress()));
-                        } else if (!replyMode){
-                            output.add(icmpRequestMessage(currentRouter.name(), currentDest.name(), sourceNode.ipAddress(), destNode.ipAddress(), 8));
-                            currentSource = currentDest;
-                            currentDest = sourceNode;
-                            replyMode = true;
-                            sourceMode = HardwareType.NODE;
-                        }
+
+                List<RouterTableLine> routerTableLines = currentRouter.routerTable().routerTableLines();
+                NetInterface netInterface = getNetInterface(currentDest, currentRouter, routerTableLines);
+
+                if (isAtSameNetwork(netInterface.ipAddress(), currentDest.ipAddress())) {
+                    if (notContainsInArpTable(currentRouter.arpTable(), currentDest.ipAddress())) {
+                        output.add(arpRequestMessage(currentRouter.name(), currentDest.ipAddress(), netInterface.ipAddress()));
+                        output.add(arpReplyMessage(currentDest.name(), currentRouter.name(), currentDest.ipAddress(), currentDest.macAddress()));
+                        currentRouter.arpTable().add(new NetInterface(currentDest.ipAddress(), currentDest.macAddress()));
+                        currentDest.arpTable().add(new NetInterface(netInterface.ipAddress(), netInterface.macAddress()));
+                    } else if (!replyMode) {
+                        output.add(icmpRequestMessage(currentRouter.name(), currentDest.name(), sourceNode.ipAddress(), destNode.ipAddress(), 8));
+                        currentSource = currentDest;
+                        currentDest = sourceNode;
+                        replyMode = true;
+                        sourceMode = HardwareType.NODE;
+                    } else {
+                        Router router = getRouter(routers, currentSource.defaultGateway());
+                        output.add(icmpReplyMessage(router.name(), currentDest.name(), destNode.ipAddress(), sourceNode.ipAddress(), 8));
+                        currentRouter = router;
+                        end = true;
                     }
                 }
-
-//                if (destNode.ipAddress().equalsIgnoreCase(currentDest.ipAddress())){
-//                    var temp = currentSource;
-//                    currentSource = currentDest;
-//                    currentDest = temp;
-//                    if (isAtSameNetwork(sourceNode.ipAddress(), currentDest.ipAddress())) {
-//                    } else {
-//                        if (notContainsInArpTable(currentSource.arpTable(), currentSource.defaultGateway())) {
-//                        } else {
-//                            Router router = getRouter(routers, currentSource.defaultGateway());
-//                            output.add(icmpReplyMessage(currentSource.name(), router.name(), destNode.ipAddress(), sourceNode.ipAddress(), 8));
-//                            currentRouter = router;
-//                        }
-//                    }
-//                }
             } else {
                 if (isAtSameNetwork(currentSource.ipAddress(), currentDest.ipAddress())) {
                     if (notContainsInArpTable(currentSource.arpTable(), currentDest.ipAddress())) {
@@ -89,6 +95,7 @@ public class Main {
                     }
                     output.add(icmpRequestMessage(sourceName, destName, currentSource.ipAddress(), currentDest.ipAddress(), 8));
                     output.add(icmpReplyMessage(destName, sourceName, currentDest.ipAddress(), currentSource.ipAddress(), 8));
+                    end = true;
                 } else {
                     if (notContainsInArpTable(currentSource.arpTable(), currentSource.defaultGateway())) {
                         Router router = getRouter(routers, currentSource.defaultGateway());
@@ -100,24 +107,16 @@ public class Main {
                                 router.arpTable().add(new NetInterface(currentSource.ipAddress(), currentSource.macAddress()));
                             }
                         }
-                    } else {
+                    } else if (!replyMode) {
                         Router router = getRouter(routers, currentSource.defaultGateway());
                         output.add(icmpRequestMessage(currentSource.name(), router.name(), sourceNode.ipAddress(), destNode.ipAddress(), 8));
                         currentRouter = router;
                         sourceMode = HardwareType.ROUTER;
-                    }
-                }
-                if(currentSource.ipAddress().equalsIgnoreCase(destNode.ipAddress()) && replyMode) {
-                    if (isAtSameNetwork(currentSource.ipAddress(), currentDest.ipAddress())) {
-
                     } else {
-                        if (notContainsInArpTable(currentSource.arpTable(), currentSource.defaultGateway())) {
-                        } else {
-                            Router router = getRouter(routers, currentSource.defaultGateway());
-                            output.add(icmpReplyMessage(currentSource.name(), router.name(), destNode.ipAddress(), sourceNode.ipAddress(), 8));
-                            currentRouter = router;
-
-                        }
+                        Router router = getRouter(routers, currentSource.defaultGateway());
+                        output.add(icmpReplyMessage(currentSource.name(), router.name(), destNode.ipAddress(), sourceNode.ipAddress(), 8));
+                        currentRouter = router;
+                        sourceMode = HardwareType.ROUTER;
                     }
                 }
             }
@@ -127,6 +126,16 @@ public class Main {
             System.out.println(item);
         }
         return output;
+    }
+
+    private static NetInterface getNetInterface(Node currentDest, Router currentRouter, List<RouterTableLine> routerTableLines) {
+        for (RouterTableLine routerTableLine : routerTableLines) {
+            if (getMask(routerTableLine.netDest()).equalsIgnoreCase(getMask(currentDest.ipAddress()))) {
+                Integer port = routerTableLine.port();
+                return currentRouter.netInterfaces().get(port);
+            }
+        }
+        return new NetInterface(currentRouter.netInterfaces().get(currentRouter.netInterfaces().size() - 1).ipAddress(), currentRouter.netInterfaces().get(currentRouter.netInterfaces().size() - 1).macAddress());
     }
 
 
--- introducao-a-redes-de-computadores/T2-Redes/src/test/java/com/t2/redes/MainTest.java
+++ introducao-a-redes-de-computadores/T2-Redes/src/test/java/com/t2/redes/MainTest.java
@@ -62,10 +62,45 @@ class MainTest {
         var routerTables = new ArrayList<RouterTable>();
         var structure = new Structure();
         structure.build(lines, nodes, routers, routerTables);
-        List run = Main.run(nodes, routers, routerTables);
+        List<String> run = Main.run(nodes, routers, routerTables, "n1", "n2");
+        assertEquals(4, run.size());
         assertEquals("Note over n1 : ARP Request<br/>Who has 192.168.0.3? Tell 192.168.0.2", run.get(0));
         assertEquals("n2 ->> n1 : ARP Reply<br/>192.168.0.3 is at 00:00:00:00:00:02", run.get(1));
         assertEquals("n1 ->> n2 : ICMP Echo Request<br/>src=192.168.0.2 dst=192.168.0.3 ttl=8", run.get(2));
         assertEquals("n2 ->> n1 : ICMP Echo Reply<br/>src=192.168.0.3 dst=192.168.0.2 ttl=8", run.get(3));
+    }
+
+    @Test
+    void test2() {
+        var file = """
+                #NODE
+                n1,00:00:00:00:00:01,192.168.0.2/24,192.168.0.1
+                n2,00:00:00:00:00:02,192.168.0.3/24,192.168.0.1
+                n3,00:00:00:00:00:03,192.168.1.2/24,192.168.1.1
+                n4,00:00:00:00:00:04,192.168.1.3/24,192.168.1.1
+                #ROUTER
+                r1,2,00:00:00:00:00:05,192.168.0.1/24,00:00:00:00:00:06,192.168.1.1/24
+                #ROUTERTABLE
+                r1,192.168.0.0/24,0.0.0.0,0
+                r2,192.168.0.0/24,0.0.0.0,0
+                r1,192.168.1.0/24,0.0.0.0,1""";
+
+        var lines = Arrays.asList(file.split("\n"));
+
+        var nodes = new ArrayList<Node>();
+        var routers = new ArrayList<Router>();
+        var routerTables = new ArrayList<RouterTable>();
+        var structure = new Structure();
+        structure.build(lines, nodes, routers, routerTables);
+        List<String> run = Main.run(nodes, routers, routerTables, "n1", "n3");
+        assertEquals(8, run.size());
+        assertEquals("Note over n1 : ARP Request<br/>Who has 192.168.0.1? Tell 192.168.0.2", run.get(0));
+        assertEquals("r1 ->> n1 : ARP Reply<br/>192.168.0.1 is at 00:00:00:00:00:05", run.get(1));
+        assertEquals("n1 ->> r1 : ICMP Echo Request<br/>src=192.168.0.2 dst=192.168.1.2 ttl=8", run.get(2));
+        assertEquals("Note over r1 : ARP Request<br/>Who has 192.168.1.2? Tell 192.168.1.1", run.get(3));
+        assertEquals("n3 ->> r1 : ARP Reply<br/>192.168.1.2 is at 00:00:00:00:00:03", run.get(4));
+        assertEquals("r1 ->> n3 : ICMP Echo Request<br/>src=192.168.0.2 dst=192.168.1.2 ttl=8", run.get(5));
+        assertEquals("n3 ->> r1 : ICMP Echo Reply<br/>src=192.168.1.2 dst=192.168.0.2 ttl=8", run.get(6));
+        assertEquals("r1 ->> n1 : ICMP Echo Reply<br/>src=192.168.1.2 dst=192.168.0.2 ttl=8", run.get(7));
     }
 }
