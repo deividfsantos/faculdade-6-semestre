404c966b9325a91cd5c8547cd2c2f6e88de2774e
Deivid Santos <deividfranciscosantos@hotmail.com>
2022-06-21 00:28:33 -0300

    atualização codigo

--- introducao-a-redes-de-computadores/T2-Redes/src/main/java/com/t2/redes/Main.java
+++ introducao-a-redes-de-computadores/T2-Redes/src/main/java/com/t2/redes/Main.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        String fileName = args[0];
-        String command = args[1];
+        var fileName = args[0];
+        var command = args[1];
         var sourceName = args[2];
         var destName = args[3];
-        FileReader fileReader = new FileReader();
+        var fileReader = new FileReader();
         var lines = fileReader.readFile(fileName);
 
         var nodes = new ArrayList<Node>();
@@ -19,10 +19,10 @@ public class Main {
         structure.build(lines, nodes, routers, routerTables);
 
         if (command.equalsIgnoreCase("ping")) {
-            Ping ping = new Ping();
+            var ping = new Ping();
             ping.run(nodes, routers, routerTables, sourceName, destName);
         } else if (command.equalsIgnoreCase("traceroute")) {
-            Traceroute traceroute = new Traceroute();
+            var traceroute = new Traceroute();
             traceroute.run(nodes, routers, routerTables, sourceName, destName);
         } else {
             System.out.println("Comando não encontrado.");
